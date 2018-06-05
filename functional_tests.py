3bfed33f2ac44fdfc3c06157764c02842863fd2a
sjy <352553647@qq.com>
2018-06-05 11:32:54 +0800

    refactor functional_test.py  add method check_for_row_in_list_table

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVistorTest(unittest.TestCase):
 	def tearDown(self):
 		self.brower.quit()
 
+	def check_for_row_in_list_table(self,row_text):
+		table = self.brower.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text,[row.text for row in rows])
+
 	def test_can_start_a_list_and_retrive_it_later(self):
 		self.brower.get('http://localhost:8000')
 		#头部和标题都包含"To-Do"这个词
@@ -33,27 +38,17 @@ class NewVistorTest(unittest.TestCase):
 		#他按回车键之后，页面更新了
 		#代办事项表格中显示了"1: Buy peacock feathers"
 		inputbox.send_keys(Keys.ENTER)
-		table = self.brower.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		# self.assertTrue(
-		# 		any(row.text == '1: Buy peacock fethers' for row in rows),
-		# 		"New to-do item dit not appear in table -- its text was:\n%s" % (table.text,)
-		# 	)
-		self.assertIn('1: Buy peacock feathers',[row.text for row in rows])
-		
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 		#页面中又显示了一个文本框，可以输入其他的代办事项
 		#她输入了"Use peacock feathers to make a fly"(使用孔雀羽毛做假蝇)
-		#伊迪斯做事很有条理
 		inputbox = self.brower.find_element_by_id('id_new_item')
 		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
-		table = self.brower.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('2: Use peacock feathers to make a fly',[row.text for row in rows])
 
 		#页面再次更新，她的清单中显示了这两个代办事项
-
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 		#伊迪斯想知道这个网站是否会记住她的清单
 		#她看到网站为她生成了一个唯一的URL
 		#而且页面中有一些文字解说这个功能
