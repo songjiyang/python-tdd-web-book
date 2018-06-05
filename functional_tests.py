a498819e86238f525ca6de591fd1c9aff18fd7c7
sjy <352553647@qq.com>
2018-06-05 11:23:05 +0800

    create POST request and corrsponding test

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 class NewVistorTest(unittest.TestCase):
 	"""docstring for NewVistorTest"""
@@ -34,14 +35,22 @@ class NewVistorTest(unittest.TestCase):
 		inputbox.send_keys(Keys.ENTER)
 		table = self.brower.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-				any(row.text == '1: Buy peacock fethers' for row in rows),
-				"New to-do item dit not appear in table"
-			)
+		# self.assertTrue(
+		# 		any(row.text == '1: Buy peacock fethers' for row in rows),
+		# 		"New to-do item dit not appear in table -- its text was:\n%s" % (table.text,)
+		# 	)
+		self.assertIn('1: Buy peacock feathers',[row.text for row in rows])
+		
 
 		#页面中又显示了一个文本框，可以输入其他的代办事项
 		#她输入了"Use peacock feathers to make a fly"(使用孔雀羽毛做假蝇)
 		#伊迪斯做事很有条理
+		inputbox = self.brower.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
+		table = self.brower.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn('2: Use peacock feathers to make a fly',[row.text for row in rows])
 
 		#页面再次更新，她的清单中显示了这两个代办事项
 
